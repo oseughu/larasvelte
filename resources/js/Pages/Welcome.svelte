b63eb79a56dd6dab2f9f5c63c6545c6b21297415
Osemudiamhen Ughu <oseughu@gmail.com>
2024-10-16 17:12:41 +0100

    test ci/cd

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -65,7 +65,7 @@
             >
             <div class="ml-4 text-lg leading-7 font-semibold">
               <a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white"
-                >Documentatio</a
+                >Documentation</a
               >
             </div>
           </div>
