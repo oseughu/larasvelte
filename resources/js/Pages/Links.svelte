fd098bfb1c92a69a69dd4a4977e2c71de39c7b34
Osemudiamhen Ughu <oseughu@gmail.com>
2024-10-19 14:55:16 +0100

    fix url input box

--- resources/js/Pages/Links.svelte
+++ resources/js/Pages/Links.svelte
@@ -67,7 +67,7 @@
 
               <div class="mt-4">
                 <InputLabel for="url" value="URL" />
-                <div class="flex w-1/2 mt-1 rounded-md shadow-sm">
+                <div class="flex w-full lg:w-1/2 mt-1 rounded-md shadow-sm">
                   <span
                     class="inline-flex items-center px-3 dark:text-gray-300 dark:bg-gray-900 rounded-l-md sm:text-sm"
                   >
@@ -76,7 +76,7 @@
                   <TextInput
                     id="url"
                     type="text"
-                    class="flex-1 block w-full min-w-0 px-3 py-2 border-gray-300 rounded-none rounded-r-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    class="flex-1 block min-w-0 px-3 py-2 border-gray-300 rounded-none rounded-r-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     bind:value={$form.url}
                     required
                     autocomplete="url"
