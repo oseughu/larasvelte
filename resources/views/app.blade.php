56ec8a3db831bca67f087814e871357b597191e1
Osemudiamhen Ughu <oseughu@gmail.com>
2024-10-27 19:45:15 +0000

    fix theme color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <!-- PWA  -->
-    <meta name="theme-color" content="#6777ef" />
-    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <!-- Chrome for Android theme color -->
     {{-- <meta name="theme-color" content="{{ $config['theme_color'] }}">
 
