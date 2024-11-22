9542c3e6058251a4361934b9cd7c12e283ea14db
Osemudiamhen Ughu <oseughu@gmail.com>
2024-11-22 15:38:02 +0000

    make database name match app name

--- config/database.php
+++ config/database.php
@@ -34,7 +34,10 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('larasvelte.sqlite')),
+            'database' => env(
+                'DB_DATABASE',
+                database_path(\Str::snake(env('APP_NAME', 'database')) . '.sqlite')
+            ),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
