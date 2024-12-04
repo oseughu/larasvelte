3f14477e483c91c450151da13ff73b3fbc5f9d88
Osemudiamhen Ughu <oseughu@gmail.com>
2024-12-04 18:40:06 +0000

    add shad-cn select component

---
+++ resources/js/Components/ui/separator/separator.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { cn } from '$/Lib/Utils.js'
+  import { Separator as SeparatorPrimitive } from 'bits-ui'
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    orientation = 'horizontal',
+    ...restProps
+  } = $props()
+</script>
+
+<SeparatorPrimitive.Root
+  bind:ref
+  class={cn(
+    'bg-border shrink-0',
+    orientation === 'horizontal' ? 'h-[1px] w-full' : 'min-h-full w-[1px]',
+    className
+  )}
+  {orientation}
+  {...restProps}
+/>
