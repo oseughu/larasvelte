55bbb394a78cc4034faf26998052a41f3c91a95c
Osemudiamhen Ughu <oseughu@gmail.com>
2024-12-10 15:44:38 +0000

    add all shadcn-svelte components

---
+++ resources/js/Components/ui/range-calendar/range-calendar-grid-row.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { RangeCalendar as RangeCalendarPrimitive } from "bits-ui";
+	import { cn } from "$/Lib/utils.js";
+
+	let { ref = $bindable(null), class: className, ...restProps } = $props();
+</script>
+
+<RangeCalendarPrimitive.GridRow bind:ref class={cn("flex", className)} {...restProps} />
