792e55303cf7a57c31c55ff50f299ab53f9f640c
Osemudiamhen Ughu <oseughu@gmail.com>
2025-01-16 00:04:03 +0000

    add api subdomain

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello API!']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
